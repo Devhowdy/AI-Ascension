21a6e2784934ab1d0cd631fc19346363792bfa8c
Devhowdy <rmhassan1021@gmail.com>
2026-04-26 05:31:05 -0500

    Replace frontend with minimal Vite starter and Vercel config

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,27 @@
-import { useMemo, useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { characters } from "./data/characters";
-import { createBattleState } from "./utils/battle";
-import Home from "./pages/Home";
-import CharacterSelect from "./pages/CharacterSelect";
-import BattleArena from "./pages/BattleArena";
+import { Routes, Route } from "react-router-dom";
 
-const defaultPlayer = characters[0];
-const defaultOpponent = characters[1];
-
-function App() {
-  const [selection, setSelection] = useState({
-    playerId: defaultPlayer.id,
-    opponentId: defaultOpponent.id,
-  });
-  const [battleState, setBattleState] = useState(() =>
-    createBattleState(defaultPlayer, defaultOpponent),
-  );
-
-  const player = useMemo(
-    () => characters.find((fighter) => fighter.id === selection.playerId) ?? defaultPlayer,
-    [selection.playerId],
-  );
-  const opponent = useMemo(
-    () =>
-      characters.find((fighter) => fighter.id === selection.opponentId) ??
-      defaultOpponent,
-    [selection.opponentId],
-  );
-
-  const updateSelection = (nextSelection) => {
-    setSelection(nextSelection);
-    const nextPlayer =
-      characters.find((fighter) => fighter.id === nextSelection.playerId) ??
-      defaultPlayer;
-    const nextOpponent =
-      characters.find((fighter) => fighter.id === nextSelection.opponentId) ??
-      defaultOpponent;
-    setBattleState(createBattleState(nextPlayer, nextOpponent));
-  };
-
-  const resetBattle = () => {
-    setBattleState(createBattleState(player, opponent));
-  };
-
+function Landing() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="/select"
-        element={
-          <CharacterSelect
-            characters={characters}
-            selection={selection}
-            onSelectionChange={updateSelection}
-          />
-        }
-      />
-      <Route
-        path="/arena"
-        element={
-          <BattleArena
-            player={player}
-            opponent={opponent}
-            battleState={battleState}
-            setBattleState={setBattleState}
-            resetBattle={resetBattle}
-          />
-        }
-      />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <div style={{textAlign:"center",marginTop:"20%"}}>
+      <h1>AI Battle Arena</h1>
+      <a href="/battle">Launch Battle</a>
+    </div>
   );
 }
 
-export default App;
+function Battle() {
+  return (
+    <div style={{textAlign:"center",marginTop:"20%"}}>
+      <h1>Battle Arena</h1>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/battle" element={<Battle />} />
+    </Routes>
+  );
+}
